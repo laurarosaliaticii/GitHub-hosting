0868f9e30408f5a5f69fb326a33b1836cc6bf597
laurarosaliaticii <59283430+laurarosaliaticii@users.noreply.github.com>
2019-12-29 14:59:54 +0100

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,175 @@
+// Image switcher code
+
+
+
+
+
+
+
+let myImage = document.querySelector('img');
+
+
+
+
+
+
+
+myImage.onclick = function() {
+
+
+
+  let mySrc = myImage.getAttribute('src');
+
+
+
+  if(mySrc === 'images/Messi.png') {
+
+
+
+    myImage.setAttribute ('src','images/Cristiano.png');
+
+
+
+  } else {
+
+
+
+    myImage.setAttribute ('src','images/Messi.png');
+
+
+
+  }
+
+
+
+}
+
+
+
+
+
+
+
+// Personalized welcome message code
+
+
+
+
+
+
+
+let myButton = document.querySelector('button');
+
+
+
+let myHeading = document.querySelector('h1');
+
+
+
+
+
+
+
+function setUserName() {
+
+
+
+  let myName = prompt('Please enter your name.');
+
+
+
+  if(!myName || myName === null) {
+
+
+
+    setUserName();
+
+
+
+  } else {
+
+
+
+    localStorage.setItem('name', myName);
+
+
+
+    myHeading.innerHTML = 'Messi es mejor, asúmelo ' + myName;
+
+
+
+  }
+
+
+
+}
+
+
+
+
+
+
+
+if(!localStorage.getItem('name')) {
+
+
+
+  setUserName();
+
+
+
+} else {
+
+
+
+  let storedName = localStorage.getItem('name');
+
+
+
+  myHeading.innerHTML = 'Messi es el mejor, asúmelo ' + storedName;
+
+
+
+}
+
+
+
+
+
+
+
+myButton.onclick = function() {
+
+
+
+  setUserName();
+
+
+
+}
+
+
+
+
+
+   var catEl = document.getElementById("cat");
+
+ 
+
+  var startTime = new Date().getTime();
+
+  var walkTheCat = function() {
+
+      var currTime = new Date().getTime();
+
+      var secondsElapsed = ((currTime - startTime)/1000);
+
+   var newLeft= secondsElapsed*77 + "px";
+
+    catEl.style.left= newLeft;
+
+    window.requestAnimationFrame(walkTheCat);
+
+  };
+
+  walkTheCat();
